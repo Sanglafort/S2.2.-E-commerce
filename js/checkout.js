cfe3a8ca65d7b0066fe728a80ea9266cc4636e1f
Sanglafort <silviaangfort@gmail.com>
2023-10-28 08:35:10 +0200

    Todos los camos obligatorios

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,39 @@
+/* De la documentacion para validaciones del bootstrap:
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	'use strict'
+  
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.querySelectorAll('.needs-validation')
+  
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms)
+	  .forEach(function (form) {
+		form.addEventListener('submit', function (event) {
+		  if (!form.checkValidity()) {
+			event.preventDefault()
+			event.stopPropagation()
+		  }
+  
+		  form.classList.add('was-validated')
+		}, false)
+	  })
+  })()
+
+*/
+
+
 
 // Exercise 6
 function validate() {
 	var error = 0;
+
+	const lettersOnly = /^[A-Za-z]+$/
+    const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,3})+$/
+    const numberFormat = /^\d{9}$/
+    const passwordFormat = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/
+	
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
@@ -26,3 +58,4 @@ function validate() {
 	}
 
 }
+
